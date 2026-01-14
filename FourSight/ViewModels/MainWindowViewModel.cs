e2e841c5bec5243503844de774f5a77a249c6594
bgn64 <bgalindo-navarro@hotmail.com>
2026-01-13 21:39:43 -0800

    Add foursight

---
+++ FourSight/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace FourSight.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
+{
+    [ObservableProperty]
+    ObservableCollection<WordItemViewModel> _wordItems;
+
+    public MainWindowViewModel()
+    {
+        WordItems = new ObservableCollection<WordItemViewModel>();
+
+        for (int row = 0; row < 4; row++)
+        {
+            for (int col = 0; col < 4; col++)
+            {
+                WordItems.Add(new WordItemViewModel()
+                {
+                    Row = row,
+                    Column = col,
+                    Text = $"Word{row}{col}"
+                });
+            }
+        }
+    }
+}
---
+++ FourSight/ViewModels/WordItemViewModel.cs
@@ -0,0 +1,15 @@
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace FourSight.ViewModels;
+
+public partial class WordItemViewModel : ViewModelBase
+{
+    [ObservableProperty]
+    int _row;
+
+    [ObservableProperty]
+    int _column;
+
+    [ObservableProperty]
+    string _text = "";
+}
